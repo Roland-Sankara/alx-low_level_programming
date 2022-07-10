2c02c5eb6342b7ff73ab0e736b2e179732ff2d0a
Roland-Sankara <roland1sankara@gmail.com>
2022-07-10 14:39:53 -0700

    update script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %s byte(s)", sizeof(char));
-	printf("Size of an int: %s byte(s)", sizeof(int));
-	printf("Size of a long int: %s byte(s)", sizeof(long));
-	printf("Size of a float: %s byte(s)", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s\n)", sizeof(long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return 0;
 }
