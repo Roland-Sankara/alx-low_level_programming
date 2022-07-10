80fae7c3abc50b34e80193e54e504b4e8e2710b0
Roland-Sankara <roland1sankara@gmail.com>
2022-07-10 13:04:09 -0700

    update script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
  * Auth Roland Sankara
  */
 #include <stdio.h>
-/*
+/**
  * main  - Prints "Programming is like building a multilingual puzzle"
  * Description: this function simply prints a string to the terminal
- * Return: 0 exit code 
+ * Return: 0 exit code
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);i
 }
