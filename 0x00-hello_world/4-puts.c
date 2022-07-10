810e8099fa35aa8e8a58599c4bd85af487e2fb1b
Roland-Sankara <roland1sankara@gmail.com>
2022-07-10 12:46:12 -0700

    update script with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**
+/*
  *
- * main  - Prints "Programming is like building a multilingual puzzle
- *  
+ * main  - Prints "Programming is like building a multilingual puzzle"
+ * Description: this function simply prints a string to the terminal
+ * Return: 0 exit code 
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");i
 	return (0);
 }
