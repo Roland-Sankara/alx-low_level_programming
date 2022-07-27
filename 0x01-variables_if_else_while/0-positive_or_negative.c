c4aff10cc3dca34ce9e81083a9707029a97a393c
Roland-Sankara <roland1sankara@gmail.com>
2022-07-27 04:13:08 -0700

    update c file to betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main  - prints negative or positive given value of n
- * Description: Function uses if and else to determine if the value of n is negative or positive
+ * Description: determines if n is negative or positive
  * Return: 0 exit code
  */
 int main(void)
@@ -11,12 +11,12 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	if(n > 0)
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
