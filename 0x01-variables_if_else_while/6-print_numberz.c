8333b15e82de0a6d5dc03fff3002bf13eb031dc8
Roland-Sankara <roland1sankara@gmail.com>
2022-07-27 14:11:38 -0700

    use putchar to print num 0 through 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints single numbers from 0
+  * Description: Prints single digit numbers from 0
+  * Return: 0 exit code
+  */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
